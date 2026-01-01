70236c21582d339e15a84c73b5cb411711089c32
Drilon Krivaqa <drilonkrivaqa555@gmail.com>
2026-01-01 17:49:27 +0100

    First commit Drilon

--- AppVegla/AppVegla/AppVegla.cpp
+++ AppVegla/AppVegla/AppVegla.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!\n";
+void showMenu() {
+    cout << "\n--- Simple App ---\n";
+    cout << "1) Add\n";
+    cout << "2) Subtract\n";
+    cout << "0) Exit\n";
+}
+
+int main() {
+    showMenu();
+    return 0;
 }
